c91d752823d1f7b90484608434e99bae8dff1c44
dLiadsk <dimaluad2@gmail.com>
2023-11-09 17:38:27 +0200

    begin

---
+++ CRM-for-culinary-masters/src/main/java/com/example/crm/CrmForCulinaryMastersApplication.java
@@ -0,0 +1,13 @@
+package com.example.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmForCulinaryMastersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmForCulinaryMastersApplication.class, args);
+	}
+
+}
