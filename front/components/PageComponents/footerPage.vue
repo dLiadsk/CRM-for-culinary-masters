01a4ea1b36000825ed9285dfd2c4d4c07ad78b44
feexq <fxqfew@gmail.com>
2023-11-26 15:19:32 +0200

    new front

---
+++ front/components/PageComponents/footerPage.vue
@@ -0,0 +1,73 @@
+<template>
+
+<footer class="footer text-center text-white" style="background-color: #f1f1f1;">
+<!-- Grid container -->
+<div class="container pt-4">
+  <!-- Section: Social media -->
+  <section class="">
+    <!-- Facebook -->
+    <a
+      class="btn btn-link btn-floating btn-lg text-dark m-1"
+      href="#!"
+      role="button"
+      data-mdb-ripple-color="dark"
+      ><i class="fab fa-facebook-f"></i
+    ></a>
+
+    <!-- Twitter -->
+    <a
+      class="btn btn-link btn-floating btn-lg text-dark m-1"
+      href="#!"
+      role="button"
+      data-mdb-ripple-color="dark"
+      ><i class="fab fa-twitter"></i
+    ></a>
+
+    <!-- Google -->
+    <a
+      class="btn btn-link btn-floating btn-lg text-dark m-1"
+      href="#!"
+      role="button"
+      data-mdb-ripple-color="dark"
+      ><i class="fab fa-google"></i
+    ></a>
+
+    <!-- Instagram -->
+    <a
+      class="btn btn-link btn-floating btn-lg text-dark m-1"
+      href="#!"
+      role="button"
+      data-mdb-ripple-color="dark"
+      ><i class="fab fa-instagram"></i
+    ></a>
+
+    <!-- Linkedin -->
+    <a
+      class="btn btn-link btn-floating btn-lg text-dark m-1"
+      href="#!"
+      role="button"
+      data-mdb-ripple-color="dark"
+      ><i class="fab fa-linkedin"></i
+    ></a>
+    <!-- Github -->
+    <a
+      class="btn btn-link btn-floating btn-lg text-dark m-1"
+      href="#!"
+      role="button"
+      data-mdb-ripple-color="dark"
+      ><i class="fab fa-github"></i
+    ></a>
+  </section>
+  <!-- Section: Social media -->
+</div>
+</footer>
+
+
+</template>
+
+<style>
+footer {
+  position: relative;
+  bottom: 0;
+}
+</style>
---
+++ front/components/profilePage.vue
@@ -0,0 +1,98 @@
+<template>
+    <main>
+      <section style="background-color: #eee;" class="wrapper">
+        <div class="container py-5">
+        <div class="row">
+          <div class="col">
+            <nav aria-label="breadcrumb" class="bg-light rounded-3 p-3 mb-4">
+              <ol class="breadcrumb mb-0">
+                <li class="breadcrumb-item active" aria-current="page"><a href="profile">User Profile</a></li>
+                <li class="breadcrumb-item active" aria-current="page"><a href="createRecipe">Create Recipe</a></li>
+              </ol>
+            </nav>
+          </div>
+        </div>
+
+  
+          <div class="row">
+            <div class="col-lg-4">
+              <div class="card mb-4 mb-md-0">
+                <div class="card-body text-center">
+                  <img :src="avatarSrc" alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
+                  <h5 class="my-3">{{ username }}</h5>
+                  <div class="d-flex justify-content-center mb-2">
+                    <button type="button" class="btn btn-primary" @click="shareLink">Share link</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card mb-4 mb-md-0">
+                <div class="card-body">
+                  <h2 class="mb-2">Recipes:</h2>
+                  <ul class="list-group list-group-flush list-unstyled">
+                    <li v-for="(recipe, index) in recipes" :key="index" class="ms-2 py-1">
+                      <input class="form-check-input me-3" type="checkbox" disabled>
+                      <a :href="`/recipe/${recipe.id}`" class="text-decoration-none text-black">{{ recipe.name }}</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card mb-4 mb-md-0">
+                <div class="card-body">
+                  <h2 class="mb-2">Menu's:</h2>
+                  <ul class="list-group list-group-flush list-unstyled">
+                    <li v-for="(menu, index) in menus" :key="index" class="ms-2 py-1">
+                      <input class="form-check-input me-3" type="checkbox" disabled>
+                      <a :href="`/menu/${menu.id}`" class="text-decoration-none text-black">{{ menu.name }}</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        avatarSrc: "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp",
+        username: "Євген Клопотенко",
+        recipes: [
+          { id: 1, name: "Перший рецепт" },
+          { id: 2, name: "Другий рецепт" },
+          { id: 3, name: "Третій рецепт" },
+          { id: 4, name: "Четвертий рецепт" },
+          { id: 5, name: "П'ятий рецепт" },
+        ],
+        menus: [
+          { id: 1, name: "Перше меню" },
+          { id: 2, name: "Друге меню" },
+          { id: 3, name: "Третє меню" },
+          { id: 4, name: "Четверте меню" },
+          { id: 5, name: "П'яте меню" },
+        ],
+      };
+    },
+    methods: {
+      shareLink() {
+        // Implement your share link logic here
+      },
+    },
+  };
+  </script>
+  
+<style scoped>
+  .wrapper {
+display: flex;
+flex-direction: column;
+min-height: 76.5vh;
+}  
+</style>
+  
